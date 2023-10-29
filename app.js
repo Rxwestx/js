347eea0308e744f481874bb11b36d7bcec4d81c5
Rxwestx <wexriexst13@gmail.com>
2023-10-29 23:39:51 +0900

    リファクタリング～ボタン

--- app.js
+++ app.js
@@ -12,10 +12,18 @@ document.getElementById("js-question").textContent = question;
 
 const $button = document.getElementsByTagName("button");
 
-$button[0].textContent = answers[0];
-$button[1].textContent = answers[1];
-$button[2].textContent = answers[2];
-$button[3].textContent = answers[3];
+// $button[0].textContent = answers[0];
+// $button[1].textContent = answers[1];
+// $button[2].textContent = answers[2];
+// $button[3].textContent = answers[3];
+
+// buttonをループ文にて再設計
+let buttonIndex = 0;
+let buttonLength = $button.length;
+while (buttonIndex < buttonLength) {
+  $button[buttonIndex].textContent = answers[buttonIndex];
+  buttonIndex++;
+}
 
 //ボタンをクリックされたら正誤判定
 document.getElementsByTagName("button")[0].addEventListener("click", () => {
@@ -25,7 +33,6 @@ document.getElementsByTagName("button")[0].addEventListener("click", () => {
     window.alert("不正解");
   }
 });
-
 document.getElementsByTagName("button")[1].addEventListener("click", () => {
   if (correct === document.getElementsByTagName("button")[1].textContent) {
     window.alert("正解!!");
@@ -33,7 +40,6 @@ document.getElementsByTagName("button")[1].addEventListener("click", () => {
     window.alert("不正解");
   }
 });
-
 document.getElementsByTagName("button")[2].addEventListener("click", () => {
   if (correct === document.getElementsByTagName("button")[2].textContent) {
     window.alert("正解!!");
