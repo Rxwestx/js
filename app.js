f57caad525a93c6e3606a63d66803773bb920012
Rxwestx <wexriexst13@gmail.com>
2023-10-29 21:34:40 +0900

    問題文追加

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+const question = "ゲーム市場、最も売れたゲーム機は次のうちどれ？";
+const answers = [
+  "スーパーファミコン",
+  "プレイステーション２",
+  "ニンテンドースイッチ",
+  "ニンテンドーDS",
+];
+const correctAnswer = "ニンテンドーDS";
 
-console.log("Hello World!");
-console.log("Hello World!");
-console.log("Hello World!");
+console.log(document.getElementsByTagName("div"));
