c143a76acf52f9dc1ae441dd44b15aada38e29bd
Rxwestx <wexriexst13@gmail.com>
2023-10-29 22:19:15 +0900

    １問目問題～ボタン配置正誤判定

--- app.js
+++ app.js
@@ -5,6 +5,47 @@ const answers = [
   "ニンテンドースイッチ",
   "ニンテンドーDS",
 ];
-const correctAnswer = "ニンテンドーDS";
+const correct = "ニンテンドーDS";
 
-console.log(document.getElementsByTagName("div"));
+//定数の文字列をHTMLに反映させる
+document.getElementById("js-question").textContent = question;
+
+const $button = document.getElementsByTagName("button");
+
+$button[0].textContent = answers[0];
+$button[1].textContent = answers[1];
+$button[2].textContent = answers[2];
+$button[3].textContent = answers[3];
+
+//ボタンをクリックされたら正誤判定
+document.getElementsByTagName("button")[0].addEventListener("click", () => {
+  if (correct === document.getElementsByTagName("button")[0].textContent) {
+    window.alert("正解!!");
+  } else {
+    window.alert("不正解");
+  }
+});
+
+document.getElementsByTagName("button")[1].addEventListener("click", () => {
+  if (correct === document.getElementsByTagName("button")[1].textContent) {
+    window.alert("正解!!");
+  } else {
+    window.alert("不正解");
+  }
+});
+
+document.getElementsByTagName("button")[2].addEventListener("click", () => {
+  if (correct === document.getElementsByTagName("button")[2].textContent) {
+    window.alert("正解!!");
+  } else {
+    window.alert("不正解");
+  }
+});
+
+document.getElementsByTagName("button")[3].addEventListener("click", () => {
+  if (correct === document.getElementsByTagName("button")[3].textContent) {
+    window.alert("正解!!");
+  } else {
+    window.alert("不正解");
+  }
+});
